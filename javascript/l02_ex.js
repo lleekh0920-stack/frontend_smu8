dd5700a0389d6f040a2e4368d977db302bdd866b
develckm86 <develckm86@gmail.com>
2026-04-22 13:10:55 +0900

    js 시ì작성법

---
+++ javascript/l02_ex.js
@@ -0,0 +1,8 @@
+//외부 스크립트!
+//node.js(v8) 를 설치하면 브라우저에서 다운받아서 실행하지 않아도됨
+console.log(`안녕~~`);
+let i=100;
+let j=200;
+console.log(i+j);
+console.log(title);
+//script 가 실행되고 title 개체가 생성되어서 찾을 수 없다
